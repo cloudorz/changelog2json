a91a89f4aa4831609aab0e0f8f82a971ede1469a
Cloud <cloudcry@gmail.com>
2019-05-05 07:36:24 +0800

    test: check parseChangelogName consume space

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -3,6 +3,7 @@ module ParserSpec where
 import Test.Hspec
 import Parser
 import Text.Megaparsec
+import Text.Megaparsec.Char
 
 spec :: Spec
 spec = do
@@ -10,4 +11,6 @@ spec = do
     describe "parse changelog Name/Title " $ do 
       it "parse \"# hello, 你好\n\"" $ do
         parse parseChangelogName "(undefined)" "# hello, 你好\n" `shouldBe` return "hello, 你好"
+      it "consume space followed by" $ do
+        parse (parseChangelogName *> char 'k') "(undefined)" "# hello, 你好\n   k" `shouldBe` return 'k'
 
