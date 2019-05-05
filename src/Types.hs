e25fcefbf53efc799e4dd29de8f5bfac04dd7e32
Cloud <cloudcry@gmail.com>
2019-05-05 20:57:44 +0800

    feat: add Eq for types

--- src/Types.hs
+++ src/Types.hs
@@ -5,17 +5,17 @@ import Data.Time.Clock (UTCTime)
 data Section = Section 
   { name :: String
   , changes :: [String]
-  } deriving (Show)
+  } deriving (Show, Eq)
                          
 data Version = Unreleased | Version 
   { version :: String
   , date :: UTCTime
   , desc :: String
   , sections :: [Section]
-  } deriving (Show)
+  } deriving (Show, Eq)
                          
 data Changelog = Changelog 
   { head :: String
   , versions :: [Version]
-  } deriving (Show)
+  } deriving (Show, Eq)
 
