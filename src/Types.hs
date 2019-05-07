83245a56b37fbbbd7142301f36d714013dc27e2c
Cloud <cloudcry@gmail.com>
2019-05-07 21:05:48 +0800

    feat: add Diff record

--- src/Types.hs
+++ src/Types.hs
@@ -1,21 +1,21 @@
 module Types where
 
-import Data.Time.Clock (UTCTime)
+data Diff = Diff { tag :: String
+                 , link :: String
+                 } deriving (Show, Eq)
 
-data Section = Section 
-  { name :: String
-  , changes :: [String]
-  } deriving (Show, Eq)
+data Section = Section { name :: String
+                       , changes :: [String]
+                       } deriving (Show, Eq)
                          
-data Version = Unreleased | Version 
-  { version :: String
-  , date :: UTCTime
-  , desc :: String
-  , sections :: [Section]
-  } deriving (Show, Eq)
+data Version = Unreleased | Version { version :: String 
+                                    , date :: String
+                                    , desc :: String
+                                    , sections :: [Section]
+                                    } deriving (Show, Eq)
                          
-data Changelog = Changelog 
-  { head :: String
-  , versions :: [Version]
-  } deriving (Show, Eq)
+data Changelog = Changelog { head :: String
+                           , versions :: [Version]
+                           , diffs :: [Diff]
+                           } deriving (Show, Eq)
 
