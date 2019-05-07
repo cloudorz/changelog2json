4fe191e768a95fd4e15da40dcc51b9997632ea82
Cloud <cloudcry@gmail.com>
2019-05-07 21:18:32 +0800

    test: check parse change entry

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -33,15 +33,30 @@ spec = do
               "All 哈 \n ## "
               `shouldBe`
               return "## "
+    describe "parse Unreleased version" $ do
       it "parse '## [Unreleased]'" $ do
         parse unreleasedVersion 
               "(undefined)"
               "## [Unreleased]  "
               `shouldBe`
               return Unreleased
+    describe "parse diff link entry" $ do
       it "parse [1.x.x]: abc568" $ do
         parse diffRecord
               "(undefined)"
               "[1.x.x]: abc568"
               `shouldBe`
               return (Diff "1.x.x" "abc568")
+    describe "parse changes entries" $ do
+      it "parse -   我是 content\n" $ do 
+        parse changesEntry
+              "(undefined)"
+              "-   我是 content\n"
+              `shouldBe`
+              return "我是 content"
+      it "parse -   我是 content" $ do 
+        parse changesEntry
+              "(undefined)"
+              "-   我是 content"
+              `shouldBe`
+              return "我是 content"
