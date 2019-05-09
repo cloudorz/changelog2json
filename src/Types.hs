2285052d2a3b186352f628d73b5f9cc23fe79062
Cloud <cloudcry@gmail.com>
2019-05-09 14:31:49 +0800

    feat: support encoding to json

--- src/Types.hs
+++ src/Types.hs
@@ -1,22 +1,39 @@
+{-# LANGUAGE DeriveGeneric #-}
 module Types where
+
+import GHC.Generics
+import Data.Aeson
+import Data.ByteString.Lazy.Char8 (unpack)
 
 data Diff = Diff { tag :: String
                  , link :: String
-                 } deriving (Show, Eq)
+                 } deriving (Generic, Show, Eq)
 
 data Section = Section { name :: String
                        , entries :: [String]
-                       } deriving (Show, Eq)
+                       } deriving (Generic, Show, Eq)
                          
 data Version = Unreleased | Version { version :: String 
                                     , date :: String
                                     , yanked :: Bool
                                     , sections :: [Section]
-                                    } deriving (Show, Eq)
+                                    } deriving (Generic, Show, Eq)
                          
 data Changelog = Changelog { head :: String
                            , desc :: String
                            , versions :: [Version]
                            , diffs :: [Diff]
-                           } deriving (Show, Eq)
+                           } deriving (Generic, Show, Eq)
+
+-- JSON encode supports
+instance ToJSON Diff where
+
+instance ToJSON Section where
+
+instance ToJSON Version where
+
+instance ToJSON Changelog where
+
+changelog2json :: Changelog -> String
+changelog2json = unpack . encode
 
