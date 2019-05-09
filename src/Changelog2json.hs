172057b45a1a5e6c45b6c56c995229f3ff1dfbfa
Cloud <cloudcry@gmail.com>
2019-05-09 11:57:26 +0800

    feat: parse change log

--- src/Changelog2json.hs
+++ src/Changelog2json.hs
@@ -2,5 +2,17 @@ module Changelog2json
     ( someFunc
     ) where
 
+import Parser
+import Types
+import Text.Megaparsec
+import Text.Megaparsec.Error
+import Data.Bifunctor (first)
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+type ChangelogError = String
+
+parseChangelog :: String -> Either ChangelogError Changelog
+parseChangelog = first errorBundlePretty <$> parse changelogParser "(undefined)"
+
